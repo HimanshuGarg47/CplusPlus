939466ee1bcc7b1124c8784000a265f180526a76
Himanshu Garg <himan.gargcoder@gmail.com>
2023-01-06 16:15:55 +0530

    add modified

--- CP/file.cpp
+++ CP/file.cpp
@@ -1,54 +1,39 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+    // your code goes here
     int t;
     cin >> t;
     while (t--)
     {
-        int n, m, k;
-        cin >> n >> m >> k;
+        int n;
+        cin >> n;
 
-        vector<int> vec(m);
+        int b, c; // a is 1
+        b = c = 0;
+        bool checknp = true;
 
-        bool equal = true;
-
-        for (int i = 0; i < m; i++)
+        for (int i = 2; i <= sqrt(n); i++)
         {
-            cin >> vec[i];
-        }
-
-        if(k == 1)
-        {
-            cout << "YES\n";
-        }
-        else if (k > m)
-        {
-            cout << "NO" << endl;
-        }
-        else if (k == m)
-        {
-            for (int i = 0; i < m - 1; i++)
+            if (n % i == 0)
             {
-                if (vec[i] != vec[i + 1])
-                {
-                    equal = false;
-                    break;
-                }
+                // If divisors are equal, print only one
+                b = i;
+                c = n / i;
+                checknp = false;
+                break;
             }
-            if (equal)
-                cout << "YES\n";
-            else
-                cout << "NO\n";
         }
-        else  // if m > k
+        if (checknp || b == c)
+            cout << -1 << endl;
+
+        else
         {
-            
+            cout << 1 << " " << b << " " << c << endl;
         }
     }
+    return 0;
 }
